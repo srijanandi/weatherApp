b84c6e3d8c35478cd22d7e1e402994ea245b07b3
srijanandi <56424698+srijanandi@users.noreply.github.com>
2023-06-03 18:06:32 +0530

    Add files via upload

---
+++ weatherApp/src/main/java/com/example/weatherApp/weatherController.java
@@ -0,0 +1,40 @@
+package com.example.weatherApp;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@RestController
+public class weatherController {
+
+	    private final WebClient webClient;
+
+	    @Value("${api.key}")
+	    private String apiKey;
+
+	    public weatherController() {
+	        this.webClient = WebClient.create();
+	    }
+//API link: http://localhost:8080/weather?cities=Pune,London
+    @GetMapping("/weather")
+    public String getWeatherForecast(@RequestParam("cities") String[] cities) {
+        StringBuilder result = new StringBuilder();
+        System.out.println(apiKey);
+        for (String city : cities) {
+            String weatherUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" +
+                    city +
+                    "&appid=" +
+                    apiKey;
+
+            String response = webClient.get()
+                    .uri(weatherUrl)
+                    .retrieve()
+                    .bodyToMono(String.class)
+                    .block();
+
+            result.append(city).append(":\n").append(response).append("\n\n");
+        }
+        return result.toString();
+    }
+    }
